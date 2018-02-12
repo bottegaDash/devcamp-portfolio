93ad6fc1369df1f28d2104ca008f9bd4859f9c51
unknown <bottegadash@gmail.com>
2018-02-11 17:06:24 -0800

    implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,14 +25,23 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolios|
+8.times do |portfolios|
   Portfolio.create!(
     title: "Portfolio title: #{portfolios}",
-    subtitle: "My great service",
+    subtitle: "Ruby on Rails",
     body: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker",
     main_image: "http://placehold.it/600x4s00",
     thumb_image: "http://placehold.it/350x200"
   )
 end
 
+1.times do |portfolios|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolios}",
+    subtitle: "Angular",
+    body: "simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker",
+    main_image: "http://placehold.it/600x4s00",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
 puts "9 portfolio items created"
